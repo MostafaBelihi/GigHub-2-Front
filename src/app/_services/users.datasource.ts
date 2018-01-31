c3bb04bdfb74b1c4a9a6911662058ea1a1c19f76
Mostafa AlBelliehy <mst.dev@gmail.com>
2018-01-31 13:47:23 +0200

    Prepare controller for user management as of reference R.4.

--- src/app/_services/users.datasource.ts
+++ src/app/_services/users.datasource.ts
@@ -7,9 +7,11 @@ import { AppUser } from "../_models/appUser.model";
 
 @Injectable()
 export class UsersDataSource {
+	serviceUrl: string = "http://localhost:5000";
+
 	constructor(private _http: Http) { }
 	
 	getUsers(): Observable<AppUser[]> {
-		return this._http.get("http://localhost:5000/api/admin").map(response => response.json());
+		return this._http.get(this.serviceUrl + "/auth/getUsers").map(response => response.json());
 	}
 }
